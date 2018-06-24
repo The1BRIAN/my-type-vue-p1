0a982553c19acd15e47bb56ab12a6ca2a4243aef
Tlangelani <mrbtmkhabela@gmail.com>
2018-06-24 19:45:38 +0200

    undemy 1

---
+++ src/components/UdemyTut.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>{{title}}
+        <div>{{another}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "landing",
+  data() {
+    return {
+      greetingsMessage: "Hello from data",
+      another:"This is another message"
+    };
+  },
+  methods: {
+    sayHelo: function() {
+      return this.greetingsMessage;
+    }
+  },
+  watch: {},
+  computed: {},
+  props: {
+    title: ""
+  }
+};
+</script>
+
+<style>
+</style>
+
